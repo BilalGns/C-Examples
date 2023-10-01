33838267f6ab9d1d2a94f26e88de5789499ad51b
BilalGns <bilalgns28@gmail.com>
2023-10-01 18:19:34 +0300

    square root calculation

---
+++ Squareroot/karakök.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int number;
+    float squareRoot;
+
+    printf("Pozitif bir sayi girin :");
+    scanf("%d", &number);
+
+    if (number < 0)
+    {
+        printf("Hatali bir sayi girdiniz...");
+    }
+
+    else
+    {
+        squareRoot = sqrt(number);
+
+        if (squareRoot * squareRoot == number)
+        {
+            printf("Girdiginiz sayinin karekoku : %.0f", squareRoot);
+        }
+
+        else
+        {
+            printf("Girdiginiz sayinin karekoku tam sayi degil...\n");
+            printf("kok %f\n", squareRoot);
+        }
+    }
+
+    return 0;
+}

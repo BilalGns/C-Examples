2d349cd051713f75a3c17c102afcd38f861d0825
BilalGns <bilalgns28@gmail.com>
2023-10-02 23:01:26 +0300

    location was changed

---
+++ C-examples/asalsayi/asal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number = 0;           
+    int i = 2;
+
+    printf("Pozitif bir sayi girin\n");
+    scanf("%d", &number);
+
+    if (number <= 1)
+    {
+        printf("Negatif sayilar , 0 ve 1 asal degildir.");
+    }
+
+    else    
+    {
+
+        while (i < number / 2)
+        {
+            if (number % i == 0)
+            {
+                printf("%d sayisi asal degildir.", number);
+                return 0;
+            }
+            i++;
+        }
+    }
+
+    printf("%d sayisi asal sayidir.", number);
+    return 0;
+}

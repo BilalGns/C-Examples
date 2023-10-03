a9f8b324069eac58bf5d5f59bb7f0fa7b6e9abb3
BilalGns <bilalgns28@gmail.com>
2023-10-03 21:54:22 +0300

    edited

--- C-examples/ortalama.c/notortalama.c
+++ C-examples/ortalama.c/notortalama.c
@@ -6,6 +6,7 @@ int main()
 
     float toplam = 0;
     float not = 0;
+    float ortalama = 0;
     int i = 1;
 
     do
@@ -33,8 +34,10 @@ int main()
 
     } while (not != 0);
 
+    ortalama = toplam / (i - 1);
+
     printf("\n%d adet not girildi.\n", (i - 1));
-    printf("not ortalamasi = %.2f ", toplam / (i - 1));
+    printf("not ortalamasi = %.2f ", ortalama);
 
     return 0;
 }
